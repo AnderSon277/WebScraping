05422b6ee45f4c8466ed283fd8605180089b9089
AnderSon2777 <anderson.cordova@epn.edu.ec>
2020-07-18 18:00:54 -0500

    Recopilation TC

--- news/news/spiders/tc_spiders.py
+++ news/news/spiders/tc_spiders.py
@@ -10,18 +10,25 @@ class NewsSpider(scrapy.Spider):
     allowed_domains = ['tctelevision.com/']
     start_urls = [
     'https://www.tctelevision.com/programa/el-noticiero',
-    'https://www.tctelevision.com/cronica'
+    'https://www.tctelevision.com/cronica',
+    'https://www.tctelevision.com/ciencia-tecnologia',
+    'https://www.tctelevision.com/comunidad',
+    'https://www.tctelevision.com/politica',
+    'https://www.tctelevision.com/tendencias',
+    'https://www.tctelevision.com/deportes',
+    'https://www.tctelevision.com/entretenimiento',
+    'https://www.tctelevision.com/tags/coronavirus'
     ]
 
     def parse(self, response):
         headlines=Selector(response).xpath('//div[@class="qt-itemcontents"]')
-        cronics=Selector(response).xpath('//div[@class="qt-part-archive-item qt-part-archive-item-inline"]')
+        titles=Selector(response).xpath('//div[@class="qt-part-archive-item qt-part-archive-item-inline"]')
         item=NewsItem()
         for headline in headlines:
             item['title']=headline.xpath('h5/a/text()').extract()[0]
             item['url']=headline.xpath('h5/a/@href').extract()[0]
             yield item
-        for cronic in cronics:
-            item['title']=cronic.xpath('h6/a/text()').extract()[0]
-            item['url']=cronic.xpath('h6/a/@href').extract()[0]
+        for title in titles:
+            item['title']=title.xpath('h6/a/text()').extract()[0]
+            item['url']=title.xpath('h6/a/@href').extract()[0]
             yield item
