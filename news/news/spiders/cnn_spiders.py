12f6ff456aaafc279e67871d42a9b06c5b183b21
AnderSon2777 <anderson.cordova@epn.edu.ec>
2020-07-18 20:06:30 -0500

    Recopilation CNN

---
+++ news/news/spiders/cnn_spiders.py
@@ -0,0 +1,36 @@
+import scrapy
+from scrapy import Spider
+from scrapy import Selector
+
+from news.items import NewsItem
+
+
+class NewsSpider(scrapy.Spider):
+    name = 'news_cnn'
+    allowed_domains = ['cnnespanol.cnn.com/']
+    start_urls = [
+    'https://cnnespanol.cnn.com/'
+    'https://cnnespanol.cnn.com/seccion/mundo/',
+    'https://cnnespanol.cnn.com/seccion/latinoamerica/',
+    'https://cnnespanol.cnn.com/seccion/estados-unidos/',
+    'https://cnnespanol.cnn.com/seccion/mundo/',
+    'https://cnnespanol.cnn.com/seccion/economia-y-negocios/',
+    'https://cnnespanol.cnn.com/seccion/entretenimiento/',
+    'https://cnnespanol.cnn.com/seccion/tecnologia/'.
+    'https://cnnespanol.cnn.com/seccion/deportes/',
+    'https://cnnespanol.cnn.com/seccion/viajes-y-turismo/',
+    'https://cnnespanol.cnn.com/seccion/salud/',
+    'https://cnnespanol.cnn.com/seccion/estilo/',
+    'https://cnnespanol.cnn.com/seccion/opinion/',
+    'https://cnnespanol.cnn.com/radio/',
+    'https://cnnespanol.cnn.com/especiales/'
+    ]
+
+    def parse(self, response):
+        headlines=Selector(response).xpath('//h2[@class="news__title"]')
+        #titles=Selector(response).xpath('//div[@class="qt-part-archive-item qt-part-archive-item-inline"]')
+        item=NewsItem()
+        for headline in headlines:
+            item['title']=headline.xpath('a/text()').extract()[0]
+            item['url']=headline.xpath('a/@href').extract()[0]
+            yield item
