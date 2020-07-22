8cf19609b5149abe99cc39f38261b473f0f64828
Ronny Cajas <ronny.cajas@epn.edu.ec>
2020-07-22 12:14:06 -0500

    el comercio spiders

--- README.md
+++ README.md
@@ -3,6 +3,10 @@
 <<<<<<< HEAD
 
 scrapy crawl news_ta -o news_ta.json -t json
+
+scrapy crawl news_co -o news_co.json -t json
+
+
 =======
 1.- Recopilar datos de la página y enviarlo a archivo json
 scrapy crawl [news..] -o [news..].json -t json
---
+++ news/news/spiders/co_spiders.py
@@ -0,0 +1,23 @@
+import scrapy
+from scrapy import Spider
+from scrapy import Selector
+
+from news.items import NewsItem
+
+
+class NewsSpider(scrapy.Spider):
+    name = 'news_co'
+    allowed_domains = ['elcomercio.com']
+    start_urls = [
+    'https://www.elcomercio.com/actualidad',
+    'https://www.elcomercio.com/data',
+    'https://www.elcomercio.com/tendencias',
+    ]
+
+    def parse(self, response):
+        headlines=Selector(response).xpath('//div[@class="article articulo-grande"]')
+        item=NewsItem()
+        for headline in headlines:
+            item['title']=headline.xpath('a/text()').extract()[0]
+            item['url']=headline.xpath('a/@href').extract()[0]
+            yield item
