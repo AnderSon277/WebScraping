d291f1022b21ad30330aa8ef557af06e0d69adf9
Ronny Cajas <ronny.cajas@epn.edu.ec>
2020-07-22 11:47:32 -0500

    scrapping con el telégrafo

--- news/news/spiders/tg_spiders.py
+++ news/news/spiders/tg_spiders.py
@@ -7,15 +7,19 @@ from news.items import NewsItem
 
 class NewsSpider(scrapy.Spider):
     name = 'news_tg'
-    allowed_domains = ['news.google.com']
+    allowed_domains = ['eltelegrafo.com.ec']
     start_urls = [
-    'https://news.google.com/',
+    'https://www.eltelegrafo.com.ec/contenido/categoria/1/politica',
+    'https://www.eltelegrafo.com.ec/contenido/categoria/1/pedro-el-economista',
+    'https://www.eltelegrafo.com.ec/contenido/categoria/1/futbol-internacional',
+    'https://www.eltelegrafo.com.ec/contenido/categoria/6/sociedad',
+    'https://www.eltelegrafo.com.ec/contenido/categoria/1/tecnologia'
     ]
 
     def parse(self, response):
-        headlines=Selector(response).xpath('//div[@class="xrnccd F6Welf R7GTQ keNKEd j7vNaf"]')
+        headlines=Selector(response).xpath('//div[@class="col-xs-8"]')
         item=NewsItem()
         for headline in headlines:
-            item['title']=headline.xpath('a/text()').extract()[0]
-            item['url']=headline.xpath('a/@href').extract()[0]
+            item['title']=headline.xpath('h2/a/text()').extract()[0]
+            item['url']=headline.xpath('h2/a/@href').extract()[0]
             yield item
